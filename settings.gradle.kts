d304c2bdd26237673b8cfe3d03f5e6e4c258a213
BeenMilk <tom34903525.12g@gmail.com>
2023-11-09 01:31:00 +0800

    CH4

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "110360118_CH4"
+include(":app")
+include(":app")
+ 
